8e1e83576a340d410f44389e9e2b7084583c94b8
Vil_Blass <jeffry16snake@hotmail.com>
2023-05-31 10:52:10 +0200

    h1,2,3

---
+++ jscrip/jscript.js
@@ -0,0 +1,11 @@
+//cargar en la ventana
+git_loock()
+function git_loock() {
+    alert("hola mundo");
+    //crear h1, h2 y h3
+    var h1 = document.createElement("h1");
+
+    var h2 = document.createElement("h2");
+    var h3 = document.createElement("h3");
+
+}
