325db66c38039fd0421e44bd1138f7b10fe4ff38
Vil_Blass <jeffry16snake@hotmail.com>
2023-05-31 10:59:21 +0200

    links

--- jscrip/jscript.js
+++ jscrip/jscript.js
@@ -4,8 +4,13 @@ function git_loock() {
     alert("hola mundo");
     //crear h1, h2 y h3
     var h1 = document.createElement("h1");
-
+    document.body.append(h1)
+    h1.textContent='buscando'
     var h2 = document.createElement("h2");
+    document.body.append(h2)
+    h2.textContent='confligtos'
     var h3 = document.createElement("h3");
+    h3.textContent='en git '
+    document.body.append(h3)
 
 }
